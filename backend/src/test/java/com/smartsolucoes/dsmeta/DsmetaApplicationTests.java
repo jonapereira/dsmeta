4994469057484428d3afac080aeeaf6f88ec3385
JONATAN PILENGHY PEREIRA <jonapereira@gmail.com>
2022-07-16 18:14:22 -0300

    Project created

---
+++ backend/src/test/java/com/smartsolucoes/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartsolucoes.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
