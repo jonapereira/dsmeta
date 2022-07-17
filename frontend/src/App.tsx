35b0f27077a5cf16965f856d8029e5fa530b933b
JONATAN PILENGHY PEREIRA <jonapereira@gmail.com>
2022-07-17 10:46:09 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,14 @@
+import NotificationButton from "./componentes/NotificationButton"
+
 function App() {
   return (
+    <>
     <h1>Olá Mundo</h1>
+    <NotificationButton/>
+    <NotificationButton/>
+    <NotificationButton/>
+    <NotificationButton/>
+    </>
   )
 }
 
