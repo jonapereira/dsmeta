350f20c481708b742924354d41ee1026514afec9
JONATAN PILENGHY PEREIRA <jonapereira@gmail.com>
2022-07-17 17:54:57 -0300

    API teste

---
+++ backend/src/main/java/com/smartsolucoes/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.smartsolucoes.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.smartsolucoes.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale,Long> {
+
+}
